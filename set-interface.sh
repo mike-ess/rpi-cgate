142d1a5306ea6262cb1b5f9fc323a8e442c85263
Mike Seales <mike@seales.co.nz>
2018-09-23 16:05:39 +1200

    Re-add set-interface.sh with Linux line-endings.

---
+++ set-interface.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Modify the project file so it will use "cni" or in other words, network connection
+# which is intended to be ser2sock on port 10001.
+perl -i'.backup' -p -e \
+"s/"\
+"<Interface><InterfaceType>.*<\/InterfaceType><InterfaceAddress>.*<\/InterfaceAddress><\/Interface>"\
+"/"\
+"<Interface><InterfaceType>cni<\/InterfaceType><InterfaceAddress>127.0.0.1:10001<\/InterfaceAddress><\/Interface>"\
+"/gi"\
+  /clipsal/cgate/tag/MY-HOME.xml
